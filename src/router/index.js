5e2e26ecdd8926cd35076ee03c1341032f7f7d9d
8rando <brandonlabrad@gmail.com>
2024-04-08 00:12:46 -0500

    Edited Files

--- src/router/index.js
+++ src/router/index.js
@@ -1,55 +1,31 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import AddMovieView from '@/views/AddMovieView.vue';
-// import { ref } from "vue"; 
-import { ref, onMounted } from "vue";
-const routes = [
-  {
-    path: '/movies/create',
-    name: 'AddMovie',
-    component: AddMovieView
-  },
-  // ... other routes
-];
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes
-});
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue')
+    },
+    {
+    path: '/forms',
+    name: 'form',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/MovieForm.vue')
+    }
+  ]
+})
 
-onMounted(() => { 
-  getCsrfToken(); 
-}); 
-
-let csrf_token = ref(""); 
-  function getCsrfToken() { 
-    fetch('/api/v1/csrf-token') 
-      .then((response) => response.json()) 
-      .then((data) => { 
-        console.log(data); 
-        csrf_token.value = data.csrf_token; 
-      }) 
-  }
-
-  function saveMovie() { 
-    let movieForm = 
-document.getElementById('movieForm'); 
-    let form_data = new FormData(uploadForm); 
-    fetch("/api/v1/movies", { 
-        method: 'POST', 
-        body: form_data, 
-        headers: { 
-            'X-CSRFToken': csrf_token.value 
-        } 
-    }) 
-    .then(function (response) { 
-        return response.json(); 
-    }) 
-    .then(function (data) { 
-        // display a success message 
-        console.log(data); 
-    }) 
-    .catch(function (error) { 
-        console.log(error); 
-    }); 
-},
-export default router;
+export default router
